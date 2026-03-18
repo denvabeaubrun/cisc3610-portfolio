04772be97d14f273d0870579ebed3b10a07a1b27
denvabeaubrun <denva.beaubrun41@bcmail.cuny.edu>
2026-03-18 03:46:31 -0400

    Update cartoon.js

--- cartoon-project/cartoon.js
+++ cartoon-project/cartoon.js
@@ -3,7 +3,7 @@ var canvas = document.getElementById("mycanvas");
 var context = canvas.getContext("2d");
 
 //Background 
-context.fillStyle = "peachpuff";
+context.fillStyle = "blue";
 context.fillRect(0, 0, canvas.width, canvas.height);
 
 
